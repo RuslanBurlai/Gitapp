cc99101b810bf35f98ccafbd779394f22b58e314
Ruslan Burlai <ruslanburlai@gmail.com>
2020-03-13 23:23:07 +0200

    Same changes

--- GitConsoleApp/Program.cs
+++ GitConsoleApp/Program.cs
@@ -1,16 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace GitConsoleApp
 {
     class Program
     {
+        static private void DeleteFile(string path)
+        {
+            File.Delete(path);
+        }
         static void Main(string[] args)
         {
+            string s = null;
             
+            Console.WriteLine(s);
+            if (s != null)
+                Console.WriteLine(s.Length);
+
+            Console.WriteLine(File.ReadAllText(@"D:\111.txt"));
+            Program product = new Program();
+            string json = JsonConvert.SerializeObject(product);
+
+            Console.WriteLine(json);
         }
     }
 }
