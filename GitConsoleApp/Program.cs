3805635c9d509bb87b57d69f6b2b4dbb0dc1ad4b
Ruslan Burlai <ruslanburlai@gmail.com>
2020-03-16 01:45:05 +0200

    Create simp;e timer

--- GitConsoleApp/Program.cs
+++ GitConsoleApp/Program.cs
@@ -1,28 +1,26 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace GitConsoleApp
 {
     class Program
     {
-        static private void DeleteFile(string path)
-        {
-            File.Delete(path);
-        }
         static void Main(string[] args)
         {
-            string s = null;
-            
-            Console.WriteLine(s);
-            if (s != null)
-                Console.WriteLine(s.Length);
-
-            Console.WriteLine(File.ReadAllText(@"D:\111.txt"));
-            Program product = new Program();
-            string json = JsonConvert.SerializeObject(product);
-
-            Console.WriteLine(json);
+            string shutdown = Console.ReadLine();
+            bool equalTime = true;
+            while (equalTime)
+            {
+                TimeSpan time = DateTime.Parse(shutdown) - DateTime.Now;                
+                if (time < TimeSpan.Zero)
+                {
+                    Process.Start("shutdown.exe", "-h");
+                    equalTime = false;
+                    Console.WriteLine(time);
+                }
+            }
         }
     }
 }
